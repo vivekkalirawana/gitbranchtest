5b91aed96764e638ed233a073c9134c15830be36
KUMAR Vivek <vivek.kumar2@soprasteria.com>
2019-03-18 17:17:10 +0530

    Added comment - master

--- src/main/java/com/example/gitbranchtest/GitbranchtestApplication.java
+++ src/main/java/com/example/gitbranchtest/GitbranchtestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitbranchtestApplication {
 
+	//Comments on master
 	public static void main(String[] args) {
 		SpringApplication.run(GitbranchtestApplication.class, args);
 	}
