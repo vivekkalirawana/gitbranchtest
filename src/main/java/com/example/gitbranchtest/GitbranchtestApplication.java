6290078bcf2d6914ece660b9dfefa23a4190602b
KUMAR Vivek <vivek.kumar2@soprasteria.com>
2019-03-18 17:13:30 +0530

    Initialize project

---
+++ src/main/java/com/example/gitbranchtest/GitbranchtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitbranchtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitbranchtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitbranchtestApplication.class, args);
+	}
+
+}
