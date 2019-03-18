d16cfeb29a38bd11d7ba505329847be9ffb8d718
KUMAR Vivek <vivek.kumar2@soprasteria.com>
2019-03-18 17:18:57 +0530

    Added comment - branch1 - comment1, comment2, comment3, comment4

--- src/main/java/com/example/gitbranchtest/GitbranchtestApplication.java
+++ src/main/java/com/example/gitbranchtest/GitbranchtestApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitbranchtestApplication {
 
+	//Added comment branch1
+	//Added comment2 branch1 - comment3 - comment4
 	public static void main(String[] args) {
 		SpringApplication.run(GitbranchtestApplication.class, args);
 	}
